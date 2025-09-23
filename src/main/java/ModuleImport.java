136211f872ce2ae5e2815cfeb0f63d586051d1ce
Nicolas Holanda <nikolas.nhm14@gmail.com>
2025-09-23 09:26:42 -0300

    Primitive type checking example

---
+++ src/main/java/ModuleImport.java
@@ -0,0 +1,11 @@
+// Import module instead of using multiple star imports
+import module java.base;
+
+public class ModuleImport {
+
+    // Compact main method without args in java 25
+    void main() {
+        Date date = new Date();
+        System.out.println("Current date: " + date);
+    }
+}
